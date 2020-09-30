1c0a685dfea584e2f02893ecaaae3e7df27be851
Zahar Bystrok <zbystrok.jr@gmail.com>
2020-09-30 11:06:24 +0300

    init: start reverse int-task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+  let str = n + ''
+  let array = str.split('')
+  if (array[0] == '-') {
+    array.shift()
+  }
+  let newArray = []
+  for(let i = array.length - 1; i >= 0; i--) {
+    newArray.push(array[i])
+  }
+  str = newArray.join('')
+  n = +str
+  return n
 }
